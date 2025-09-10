d172163a245394eec04895c65ad11055fe069c15
Ariansch <ariansch.kakkar@stud.leuphana.de>
2025-09-10 23:34:42 +0200

    added enum for authentication types

--- CarReservation/src/authentication/structure/Credential.java
+++ CarReservation/src/authentication/structure/Credential.java
@@ -1,23 +1,24 @@
 package authentication.structure;
 
 public class Credential {
-	//Type of credential, e.g., "password", fingerprint, "eye scan"
-	private String type;
-	
-	//Value of the credential, e.g., actual password, fingerprint data, eye scan data
-	private String value;
-	
-	public Credential(String type, String value) {
+	// Type of credential, e.g., "password", fingerprint, "eye scan"
+	private final CredentialType type;
+
+	// Value of the credential, e.g., actual password, fingerprint data, eye scan
+	// data
+	private final String value;
+
+	public Credential(CredentialType type, String value) {
 		this.type = type;
 		this.value = value;
 	}
-	
-	public String getType() {
+
+	public CredentialType getType() {
 		return type;
 	}
-	
+
 	public String getValue() {
 		return value;
 	}
-	
+
 }
---
+++ CarReservation/src/authentication/structure/CredentialType.java
@@ -0,0 +1,6 @@
+package authentication.structure;
+
+// Enum for different types of Autjhentication Credentials
+public enum CredentialType {
+	PASSWORD, FINGERPRINT, EYESCAN
+}
