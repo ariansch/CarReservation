ca6a6677bafe293fe3b829d38ae7fcf7a428d58a
albidzn <albimehana@hotmail.com>
2025-09-14 17:41:34 +0200

    Adjusted to API calls for resource prices

--- CarReservation/tests/tests/BookingServiceTest.java
+++ CarReservation/tests/tests/BookingServiceTest.java
@@ -29,15 +29,15 @@ public class BookingServiceTest {
 		personService.createPerson("legal", "person1");
 		personService.createPerson("natural", "person2");
 		
-		resourceService.addResource(new Resource("Resource1"));
-		resourceService.addResource(new Resource("Resource2"));
+		resourceService.addResource(new Resource("Resource1", 100.0));
+		resourceService.addResource(new Resource("Resource2", 200.0));
 	}
 	
 	@Test
 	// Testen, ob bei einer deutschen Buchung Header, Body und Footer die erwarteten Informationen enthalten
 	public void isGermanBookingRendered() {
 		// Sprache Deutsch
-		bookingService.createBooking("DE", "1", "person1", "Resource1", 100.0);
+		bookingService.createBooking("DE", "1", "person1", "Resource1");
 		
 		Booking b = bookingService.getBookingById("1");
 		assertNotNull(b, "Booking should exist after creation");
@@ -58,7 +58,7 @@ public class BookingServiceTest {
 	@Test
 	// Testen, ob bei einer englischen Buchung Header, Body und Footer die erwarteten Informationen enthalten
 	public void isEnglishBookingRendered() {
-		bookingService.createBooking("EN", "2", "person2", "Resource2", 200.0);
+		bookingService.createBooking("EN", "2", "person2", "Resource2");
 		
 		Booking b = bookingService.getBookingById("2");
 		assertNotNull(b, "Booking should exist after creation");
@@ -79,8 +79,8 @@ public class BookingServiceTest {
 	@Test
 	// Testen, ob eine Buchung gelöscht werden kann und danach nicht mehr gefunden wird
 	public void isBookingDeletedAndLookupFails() {
-		bookingService.createBooking("DE", "3", "person1", "Resource1", 150.0);
-		bookingService.createBooking("EN", "4", "person2", "Resource2", 250.0);
+		bookingService.createBooking("DE", "3", "person1", "Resource1");
+		bookingService.createBooking("EN", "4", "person2", "Resource2");
 		
 		bookingService.deleteBooking("3");
 		String msg = bookingService.getBookingBodyById("3");
@@ -92,7 +92,7 @@ public class BookingServiceTest {
 	@Test
 	// Testen, ob der Footer einer Buchung korrekt lokalisiert wird
 	public void isBookingFooterLocalized() {
-		bookingService.createBooking("EN", "5", "person2", "Resource2", 300.0);
+		bookingService.createBooking("EN", "5", "person2", "Resource2");
 		String footer = bookingService.getBookingFooterById("5");
 		assertTrue(footer.contains("Thank you"), "English Footer expected");
 	}
@@ -100,9 +100,9 @@ public class BookingServiceTest {
 	@Test
 	// Testen, ob das Erstellen einer Buchung mit doppelter ID eine Exception wirft
 	public void isDuplicateBookingIdRejected() {
-		bookingService.createBooking("DE", "6", "person1", "Resource1", 350.0);
+		bookingService.createBooking("DE", "6", "person1", "Resource1");
 		try {
-			bookingService.createBooking("EN", "6", "person2", "Resource2", 400.0);
+			bookingService.createBooking("EN", "6", "person2", "Resource2");
 		} catch (IllegalArgumentException e) {
 			assertEquals("Booking with id 6 already exists", e.getMessage());
 		}
